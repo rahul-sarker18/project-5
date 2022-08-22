4dae29ad1bb2d18655e1b7bdb4ffe7d32bf4c5ff
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-08-22 12:25:59 +0530

    player expenses add

--- ja/script.js
+++ ja/script.js
@@ -8,18 +8,19 @@ function displayProduct(){
     for(let i =0; i < cart.length; i++){
 
         const tr = document.createElement("tr");
-        tr.innerHTML = `
-        <th>${i+1}</th>
-        <td>${cart[i].pdName}</td>
-        `;
-        cartContainer.appendChild(tr);
+       
+       
+            tr.innerHTML = `
+            <th>${i+1}</th>
+            <td>${cart[i].pdName}</td>
+            `;
 
+        cartContainer.appendChild(tr);
     }
 }
 
 function addToCart(element){
     const pdName = element.parentNode.parentNode.children[0].innerText;
-    console.log(pdName);
 
     const pd ={
         pdName: pdName
@@ -28,4 +29,28 @@ function addToCart(element){
     console.log(cart);
 
     displayProduct();
+}
+
+
+
+// calclutar section stard 
+
+function calclutar(){
+    const parPlayer =document.getElementById('par-player');
+    const parPlayertring = parPlayer.value;
+    const parPlayerNumber = parseInt(parPlayertring);
+    parPlayer.value ='';
+
+    // td.textContent ='';
+    // const td = tr.children[0].length.innerText;
+    // const tdnumber =parseInt(td);
+
+    const allPlayer = parPlayerNumber * 5;
+
+    console.log(allPlayer)
+
+    const playerCosd =document.getElementById('player-cosd');
+    const allPlayerCosd = playerCosd.innerText;
+    
+    playerCosd.innerText =allPlayer;
 }
